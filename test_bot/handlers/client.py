6bb1a7245529b51a88177f0fe903e91ac377bdaa
Careprop <97297596+Careprop@users.noreply.github.com>
2022-08-02 02:44:37 +0300

    Update client.py

--- test_bot/handlers/client.py
+++ test_bot/handlers/client.py
@@ -2,7 +2,7 @@ from aiogram import types, Dispatcher
 from create_bot import dp, bot
 
 
-#@dp.message_handler(commands=['start'])
+#@dp.message_handler(commands=['help'])
 async def command_start(message: types.message):
     pass
 
